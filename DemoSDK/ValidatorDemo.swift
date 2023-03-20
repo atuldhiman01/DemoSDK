b2a82636e234ab2c5aeac6d827aae68bad8776e0
atu1dhiman <112083665+atuldhiman01@users.noreply.github.com>
2023-03-20 13:11:46 +0530

    Swift File Add

---
+++ DemoSDK/ValidatorDemo.swift
@@ -0,0 +1,14 @@
+//
+//  ValidatorDemo.swift
+//  ValidatorDemoSDK
+//
+//  Created by Atul Dhiman on 20/03/23.
+//
+
+import Foundation
+
+public struct ValidatorDemo {
+    public static func validate() {
+        print("Demo Purpose for deploying pods. Will add Functionality to this pod later.")
+    }
+}
